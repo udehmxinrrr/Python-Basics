ace30040d5e342e1d224012dd40ba9279ea18af8
udehmxinrrr@gmail.com <udehmxinrrr@gmail.com>
2026-02-03 10:26:03 +0300

    Pyhton Basics

---
+++ Introduction.py
@@ -0,0 +1,6 @@
+print("Welcome to Python Programming")
+print("Udeh")
+print(100)
+print(20*3)
+
+#Print is a method used to display an output. A method (such as print) is always accompanied by parentheses
---
+++ Variables.py
@@ -0,0 +1,16 @@
+#A variable is just a container user to store a value: eg;
+
+school = "eMobilis"
+firstname = "Udeh"
+lastname = "Mukeni"
+num1 = 56
+num2 = 98
+language = "Python"
+
+print(school)
+print(firstname, lastname)
+print(num1 + num2)
+
+print(language,"is a web programming language")
+
+print("The sum of",num1,"and",num2,"is",num1 + num2)
